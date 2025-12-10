5f96d97f77d6d33ecbbef9a95d731d29e1a68367
madilfarooq7 <madilfarooq7@gmail.com>
2025-12-10 16:44:36 +0500

    Create config.php
    email host

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+// config.php
+
+// SMTP Server Settings
+$SMTP_HOST = "mail.itsolutionshub2010.com";
+$SMTP_PORT = 587; // Usually 587 for TLS or 465 for SSL
+$SMTP_USER = "noreply@itsolutionshub2010.com";
+$SMTP_PASS = "P@ssw0rdEmail"; // Use App Password if using Gmail/Outlook
+
+// Sender
+$FROM_EMAIL = "noreply@itsolutionshub2010.com";
+$FROM_NAME = "No Reply ITSH";
+?>
