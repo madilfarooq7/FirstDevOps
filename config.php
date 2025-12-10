2ac987d656b4facd2b0fcf606e367d428cd81bc3
madilfarooq7 <madilfarooq7@gmail.com>
2025-12-10 20:58:51 +0500

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
 $SMTP_HOST = "mail.itsolutionshub2010.com";
 $SMTP_PORT = 587; // Usually 587 for TLS or 465 for SSL
 $SMTP_USER = "noreply@itsolutionshub2010.com";
-$SMTP_PASS = "P@ssw0rdEmail"; // Use App Password if using Gmail/Outlook
+$SMTP_PASS = ""; // Use App Password if using Gmail/Outlook
 
 // Sender
 $FROM_EMAIL = "noreply@itsolutionshub2010.com";
